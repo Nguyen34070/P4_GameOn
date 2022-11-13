3408ad4d201df331a81f9eebc921f781e409e0ae
Nguyen Thanh <nguyen34070@gmail.com>
2022-11-13 19:16:42 +0100

    first commit

---
+++ modal.js
@@ -0,0 +1,32 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const btnSubmit = document.querySelector('.btn-submit');// a voir 
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+spanValid
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Pour fermer le formulaire avec l'icone croix
+function closeModal() {
+  document
+    .getElementById('closed')
+    .addEventListener("click", function() {
+      modalbg.style.display = "none";  
+  });
+}
